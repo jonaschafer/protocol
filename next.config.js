e242acf948bfb0fad112702a8650d9be55b7eae6
jonaschafer <jonaschafer@gmail.com>
2026-01-17 08:27:56 -0800

    Move protocol-local and update ignore files
    - Move protocol-local folder into protocol directory
    - Add protocol-local to .gitignore, .vercelignore, and tsconfig.json exclude
    - Update next.config.js to exclude protocol-local from webpack compilation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Exclude archive and local directories from webpack compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/node_modules/**',
+        '**/protocol-archive/**',
+        '**/protocol-archive-old/**',
+        '**/protocol-local/**',
+        '**/swoletron-archive/**',
+      ],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
